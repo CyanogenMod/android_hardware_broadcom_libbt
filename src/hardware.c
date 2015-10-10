d3a277bd8bfa08ae80b71e5d8f0d632e94887ce5
Dmitry Grinberg <dmitrygr@gmail.com>
2015-10-10 10:10:11 +0000

    Avoid an annoying bug that only hits BCM chips running at less than 3MBps
    Change-Id: If31e05cd4789660780b4f34288c0d4023554afe6

--- src/hardware.c
+++ src/hardware.c
@@ -878,8 +878,10 @@ void hw_config_cback(void *p_mem)
     } // if (p_buf != NULL)
 
     /* Free the RX event buffer */
-    if (bt_vendor_cbacks)
-        bt_vendor_cbacks->dealloc(p_evt_buf);
+
+//on manta this causes a crash due to an overrun elsewhere. Sad as it is, if we just do not do the free here we'll just leak less than 4K each time BT is turned on. And since when it is turned off, the process dies, this realy only costs us 4K total. I'm willing to part with 4K to avoid debugging bluedroid
+//    if (bt_vendor_cbacks)
+//        bt_vendor_cbacks->dealloc(p_evt_buf);
 
     if (is_proceeding == FALSE)
     {
